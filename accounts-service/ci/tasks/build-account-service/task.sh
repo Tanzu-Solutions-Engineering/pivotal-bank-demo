da8fb89724f08f6098ad465ce0c343c9077356b9
Dodd Pfeffer <dpfeffer@pivotal.io>
2018-07-24 09:58:16 -0400

    Updates account services ci scripts

--- accounts-service/ci/tasks/build-account-service/task.sh
+++ accounts-service/ci/tasks/build-account-service/task.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-pushd accounts-service
-    pwd
-    ls -ltr
-    ./gradlew clean assemble
-#
-#    VERSION=`cat version-number`
-popd
+#pushd accounts-service
+#    pwd
+#    ls -ltr
+#    ./gradlew clean assemble
+##
+##    VERSION=`cat version-number`
+#popd
 
-mkdir build-output/libs && cp accounts-service/accounts-service/build/libs/$ARTIFACT_ID.jar build-output/libs/.
-cp accounts-service/manifest.yml build-output/.
+./accounts-service/accounts-service/gradlew clean assemble
+
+#mkdir build-output/libs && cp accounts-service/accounts-service/build/libs/$ARTIFACT_ID.jar build-output/libs/.
+#cp accounts-service/accounts-service/manifest.yml build-output/.
