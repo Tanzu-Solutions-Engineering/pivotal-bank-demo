251852c5addf338fd17e20b7829d90b2a9246594
Dodd Pfeffer <dpfeffer@pivotal.io>
2018-07-24 11:09:15 -0400

    Updates account services ci scripts

--- accounts-service/ci/tasks/build-account-service/task.sh
+++ accounts-service/ci/tasks/build-account-service/task.sh
@@ -15,5 +15,10 @@ ls -ltr accounts-service/accounts-service/build/libs
 
 #./accounts-service/accounts-service/gradlew clean assemble
 
+echo "Making build-output/libs directory and copying jar"
 mkdir build-output/libs && cp accounts-service/accounts-service/build/libs/$ARTIFACT_ID.jar build-output/libs/.
+echo "Copying manifest to build-output directory"
 cp accounts-service/accounts-service/manifest.yml build-output/.
+echo "list build-output folder contents"
+ls -ltr build-output
+ls -ltr build-output/libs
