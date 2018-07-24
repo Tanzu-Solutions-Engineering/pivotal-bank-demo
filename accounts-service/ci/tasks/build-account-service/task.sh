a855c46f832eb85f47f0075466104b62b7fc4873
Dodd Pfeffer <dpfeffer@pivotal.io>
2018-07-24 09:50:48 -0400

    Updates account services ci scripts

--- accounts-service/ci/tasks/build-account-service/task.sh
+++ accounts-service/ci/tasks/build-account-service/task.sh
@@ -10,4 +10,4 @@ pushd accounts-service
 popd
 
 mkdir build-output/libs && cp accounts-service/accounts-service/build/libs/$ARTIFACT_ID.jar build-output/libs/.
-cp accounts-service/build/manifest.yml build-output/.
+cp accounts-service/manifest.yml build-output/.
