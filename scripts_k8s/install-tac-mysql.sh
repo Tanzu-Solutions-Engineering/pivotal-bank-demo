75942e5a8e2a4eea657474f8a7b2822eff8ecede
arau <arau@pivotal.io>
2020-06-23 15:20:33 -0400

    fixed mysql install

--- scripts_k8s/install-tac-mysql.sh
+++ scripts_k8s/install-tac-mysql.sh
@@ -2,5 +2,5 @@
 
 # Need to have helm3 installed for this to work
 helm repo add tacDemo https://charts.trials.tac.bitnami.com/demo
-heml repo update
+helm repo update
 helm install acme-mysql tacDemo/mysql --version 6.14.0 --set useHelm3=true
